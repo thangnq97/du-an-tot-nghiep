256af8efa685f720459fb6803665b31e3aeeeda5
thangnq97 <thangmanu.97@gmail.com>
2024-02-17 21:52:15 +0700

    Thang: Update migration

--- database/migrations/2024_02_16_162124_create_services_table.php
+++ database/migrations/2024_02_16_162124_create_services_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('servies', function (Blueprint $table) {
+        Schema::create('services', function (Blueprint $table) {
             $table->id();
             $table->string('name', 255);
             $table->integer('price');
