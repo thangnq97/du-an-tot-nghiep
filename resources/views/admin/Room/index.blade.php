0c6e7974450149121581dceeb1ab10354d5615c2
LeHoii <levanhoi.backenddeveloper@gmail.com>
2024-02-28 22:29:56 +0700

    Update giao diện mới

--- resources/views/admin/Room/index.blade.php
+++ resources/views/admin/Room/index.blade.php
@@ -12,24 +12,28 @@
         <a href="" class="btn btn-success room-button">Sửa Phòng</a>
         <a href="" class="btn btn-danger room-button">Xóa Phòng</a>
     </div>
-    <div class="row">
+    <div class="row ">
         @foreach ($data as $query)
-            <div class="card card-room" style="width: 15rem;">
-                <div class="card-body">
-                    <h5 class="card-title"><i class="fa-solid fa-house"> {{ $query->name }}</i></h5>
-                    <h6><a href="{{ route('room.createpeople') }}" class="btn btn-primary text-room ">Thêm Khách</a></h6>
-                    <h6 class="card-text text-room">Số lượng : {{ $query->member_quantity }}</h6>
-                    <h6 class="card-text text-room"> Số lượng giới hạn : {{ $query->member_maximum }}</h6>
-                    <h6 class="card-text text-room"> Giá tiền : {{ number_format($query->price) }}</h6>
-                    <div class="action-button">
-                        <a href="{{ route('room.edit', $query) }}" class="btn btn-primary button-action">
-                            <i class="fa-regular fa-pen-to-square"></i></a>
-                        <form action="{{ route('room.destroy', $query) }}" method="POST">
-                            @csrf
-                            @method('DELETE')
-                            <button class="btn btn-danger button-action" type="submit" onclick="return confirm('Bạn có muốn xóa không')">
-                                <i class="fa-solid fa-trash-can"></i></button>
-                        </form>
+            <div class="col-3">
+                <div class="card card-room" style="width: 15rem;">
+                    <div class="card-body">
+                        <h4 class="card-title"><i class="fa-solid fa-house"> {{ $query->name }}</i></h4>
+                        <h6><a href="{{ route('room.createpeople') }}" class="btn btn-primary text-room ">Thêm Khách</a>
+                        </h6>
+                        <h6 class="card-text text-room">Số lượng : {{ $query->member_quantity }}</h6>
+                        <h6 class="card-text text-room"> Số lượng giới hạn : {{ $query->member_maximum }}</h6>
+                        <h6 class="card-text text-room"> Giá tiền : {{ number_format($query->price) }}</h6>
+                        <div class="action-button">
+                            <a href="{{ route('room.edit', $query) }}" class="btn btn-success button-action">
+                                <i class="fa-regular fa-pen-to-square"></i></a>
+                            <form action="{{ route('room.destroy', $query) }}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button class="btn btn-danger button-action" type="submit"
+                                    onclick="return confirm('Bạn có muốn xóa không')">
+                                    <i class="fa-solid fa-trash-can"></i></button>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
